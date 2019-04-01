4c79ba541406f61ff4f466d17da807a223dfaa60
JessieKate <lyj1246505807@gmail.com>
2019-04-01 21:06:38 +0800

    修改获取漫画内容接口，兼容水平浏览漫画

--- server/controller/netease/content.js
+++ server/controller/netease/content.js
@@ -6,6 +6,7 @@
 let websocket = require('../websocketutil')
 let Spider = require('../spider')
 let page
+let result2
 //　获取漫画内容
 exports.getComicContent = async function (url) {
     page = await Spider.init()
@@ -13,7 +14,14 @@ exports.getComicContent = async function (url) {
     // 跳转到目标网站
     await page.goto(url)
     console.log('catch------>', url)
-    return await this.getImgs()
+    let portrait = await page.$('div.portrait-player .img-box')
+    if (portrait){
+        return await this.getImgs()
+    }else{
+        result2 = []
+        return await this.getImgs2()
+    }
+
 }
 
 //　获取上一话和下一话的内容
@@ -78,4 +86,47 @@ exports.getImgs = async function () {
     console.log({data, loadMore,imgWidth, imgHeight})
     return {data, loadMore,imgWidth, imgHeight}
 
+}
+
+/**
+ * 个别漫画的抓取方式（水平左右布局）
+ * @returns {Promise.<*>}
+ */
+exports.getImgs2 = async function() {
+    // 等待
+    await page.waitFor(700)
+    let length = await  page.$eval('span.js-totalIndex', text => text.innerText)
+    let index = await page.$eval('span.js-currentIndex', text => text.innerText)
+    websocket.curSocket.emit('catch',{index:index,length:length})
+    // 可能有提示弹窗
+    let closeNode = await page.$('div.close')
+    if (closeNode){
+        closeNode.click()
+    }
+    let leftNode = await page.$('div.img-box.img-box-leftin img')
+    if (leftNode){
+        let leftSrc = await page.$eval('div.img-box.img-box-leftin img', img => img.src)
+        result2.push(leftSrc)
+    }
+    let rightNode =  await page.$('div.img-box.img-box-rightin img')
+    if (rightNode){
+        let rightSrc = await page.$eval('div.img-box.img-box-rightin img', img => img.src)
+        result2.push(rightSrc)
+    }
+    let endNode = await page.$('div.rd-locked-popup.a-in')
+    // 到结尾了
+    if (endNode || (!leftNode && !rightNode)){
+        console.log(result2)
+        return {data: result2 ,loadMore: true, imgWidth: 617, imgHeight:960}
+    }else{
+        // 点击下一页抓取
+        await this.clickPage()
+    }
+}
+
+// 点击下一页
+exports.clickPage = async function () {
+    await page.mouse.move(Spider.viewPort.width/3, Spider.viewPort.height/2, {steps: 10})
+    await page.mouse.click(Spider.viewPort.width/3, Spider.viewPort.height/2)
+    await this.getImgs2()
 }

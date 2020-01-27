9ba892a289904645021bcb3ea9ecb3c8e2281e66
JessieKate <lyj1246505807@gmail.com>
2020-01-27 15:58:55 +0800

    获取漫画更多详情接口修改

--- server/controller/netease/detail.js
+++ server/controller/netease/detail.js
@@ -4,7 +4,97 @@
  * @description : 获取网易漫画详情
  */
 let Spider = require('../spider')
-// 获取漫画详情(章节)
+
+/**
+ * 获取漫画章节
+ * @param page
+ * @returns {Promise<[]>}
+ */
+async function getComicItems(page) {
+    let links = await page.$$eval('a.episode-item', ls => {
+        const temp = []
+        ls.forEach(async link => {
+            temp.push(link.getAttribute('href'))
+        })
+        return temp
+    })
+    let items = await page.$$eval('a.episode-item div.item-body', texts => {
+        const temp = []
+        texts.forEach(async text => {
+            temp.push(text.innerText)
+        })
+        return temp
+    })
+    let data = []
+    for (let i = 0; i < links.length; i++) {
+        let temp = {}
+        temp.link = links[i]
+        temp.text = items[i]
+        data.push(temp)
+    }
+    return data
+}
+
+/**
+ * 获取漫画评论
+ * @param page
+ * @returns {Promise<[]>}
+ */
+async function getCommentItems(page) {
+    let commUserAvatars = await page.$$eval('div.user-info span.user-avatar', avatars => {
+        const temp = []
+        avatars.forEach(async avatar => {
+            temp.push(avatar.getAttribute('style')
+                .replace('background-image: url(', '')
+                .replace(/\"/g, "")
+                .replace(');', '')
+                .substring(2))
+        })
+        return temp
+    })
+
+    let commUserNames = await page.$$eval('div.user-info p.user-name', userNames => {
+        const temp = []
+        userNames.forEach(async userName => {
+            temp.push(userName.innerText)
+        })
+        return temp
+    })
+
+    let commTimes = await page.$$eval('div.user-info p.create-time', times => {
+        const temp = []
+        times.forEach(async time => {
+            temp.push(time.innerText)
+        })
+        return temp
+    })
+
+    let commInfos = await page.$$eval('div.comment-info div', infos => {
+        const temp = []
+        infos.forEach(async info => {
+            temp.push(info.getAttribute('title'))
+        })
+        return temp
+    })
+
+    // 封装后返回
+    let commentItem = []
+    for (let i = 0; i < commUserAvatars.length; i++) {
+        let temp = {}
+        temp.avatar = commUserAvatars[i]
+        temp.name = commUserNames[i]
+        temp.time = commTimes[i]
+        temp.commentText = commInfos[i]
+        commentItem.push(temp)
+    }
+    return commentItem
+}
+
+/**
+ * 获取漫画详情
+ * @param url
+ * @returns {Promise<{cover: *, timeInfo: *, data: *, author: *, style: *, id: *, title: *, evaluate: *, commentItem: *}>}
+ */
 exports.getComicDetail = async function (url) {
     let page = await Spider.init()
     await Spider.switchMobile(page)
@@ -42,79 +132,17 @@ exports.getComicDetail = async function (url) {
     let evaluate = await page.$eval('div.evaluate span', text => {
         return text.innerText
     })
-    let links = await page.$$eval('a.episode-item', ls => {
-        const temp = []
-        ls.forEach(async link => {
-            temp.push(link.getAttribute('href'))
-        })
-        return temp
-    })
-    let items = await page.$$eval('a.episode-item div.item-body', texts => {
-        const temp = []
-        texts.forEach(async text => {
-            temp.push(text.innerText)
-        })
-        return temp
-    })
-    let data = []
-    for (let i = 0; i< links.length; i++){
-        let temp = {}
-        temp.link = links[i]
-        temp.text = items[i]
-        data.push(temp)
-    }
-    let commUserAvatars = await page.$$eval('div.user-info span.user-avatar', avatars => {
-        const temp = []
-        avatars.forEach(async avatar => {
-            temp.push(avatar.getAttribute('style')
-                .replace('background-image: url(','')
-                .replace(/\"/g, "")
-                .replace(');','')
-                .substring(2))
-        })
-        return temp
-    })
-
-    let commUserNames = await page.$$eval('div.user-info p.user-name', userNames => {
-        const temp = []
-        userNames.forEach(async userName => {
-            temp.push(userName.innerText)
-        })
-        return temp
-    })
-
-    let commTimes = await page.$$eval('div.user-info p.create-time', times => {
-        const temp = []
-        times.forEach(async time => {
-            temp.push(time.innerText)
-        })
-        return temp
-    })
-
-    let commInfos = await page.$$eval('div.comment-info div', infos => {
-        const temp = []
-        infos.forEach(async info => {
-            temp.push(info.getAttribute('title'))
-        })
-        return temp
-    })
-
-    // 封装后返回
-    let commentItem = []
-    for(let i = 0; i< commUserAvatars.length; i++) {
-        let temp = {}
-        temp.avatar = commUserAvatars[i]
-        temp.name = commUserNames[i]
-        temp.time = commTimes[i]
-        temp.commentText = commInfos[i]
-        commentItem.push(temp)
-    }
+    let data = await getComicItems(page)
+    let commentItem = await getCommentItems(page)
 
 
     return {cover,title,author,style,id,timeInfo,evaluate,data,commentItem}
 }
 
-// 获取漫画详情所有章节
+/**
+ * 获取漫画详情所有章节
+ * @returns {Promise<*[]>}
+ */
 exports.getComicDetailMore = async function (){
     let page = Spider.lastPage()
 
@@ -124,26 +152,6 @@ exports.getComicDetailMore = async function (){
     }
     // 等待
     await page.waitFor(200)
-    let links = await page.$$eval('a.episode-item', ls => {
-        const temp = []
-        ls.forEach(async link => {
-            temp.push(link.getAttribute('href'))
-        })
-        return temp
-    })
-    let items = await page.$$eval('a.episode-item div.item-body', texts => {
-        const temp = []
-        texts.forEach(async text => {
-            temp.push(text.innerText)
-        })
-        return temp
-    })
-    let data = []
-    for (let i = 0; i< links.length; i++){
-        let temp = {}
-        temp.link = links[i]
-        temp.text = items[i]
-        data.push(temp)
-    }
+    let data = await getComicItems(page)
     return data.splice(7, data.length)
 }
